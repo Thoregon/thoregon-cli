1211092c24417241abfa878889378335bdb31d5c
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-01-06 21:37:49 +0100

    work on schemabuilder, boundedcontextbuilder, test case subscriberlist

---
+++ cli/scope.mjs
@@ -0,0 +1,9 @@
+/**
+ *
+ *
+ * @author: Bernhard Lukassen
+ */
+
+import newScope from "../lib/scope.mjs";
+
+console.log(`Copy the following id to the 'universe.*.mjs' config as -> protouniverse.scope = '${newScope()}'`);
